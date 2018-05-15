865d00b2227f80a5cc9d9fef787543e9503bc829
xiaoqiang_087 <zhangxiaoqiangjoe@gmail.com>
2018-05-15 17:39:12 +0800

    Initial Commit

---
+++ CircleListViewDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CircleListViewDemo
+//
+//  Created by 张小强 on 2018/5/15.
+//  Copyright © 2018年 张小强. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
